26b4e32cd758e5a6dbd36772ba2519b784910456
Kriengsak Ko <kriengsak.ko@kbtg.tech>
2019-05-16 23:43:11 +0700

    add balance checking, transaction validation, mining reward

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
 # global scope variable - can call by anywhere
+MINING_REWARD = 10
+
 genesis_block = {
     'previous_hash': '',
     'index': 0,
@@ -12,6 +14,29 @@ participants = {'Golf'}
 
 def hash_block(block):
     return ''.join([str(block[key]) for key in block])
+
+
+def get_balance(participant):
+    tx_sender = [[tx['amount'] for tx in block['transactions']
+                  if tx['sender'] == participant] for block in blockchain]
+    open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
+    tx_sender.append(open_tx_sender)
+    amount_sent = 0
+    for tx in tx_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions']
+                     if tx['recipient'] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in tx_recipient:
+        if len(tx) > 0:
+            amount_received += tx[0]
+    return amount_received - amount_sent
+
+
+def verify_transaction(transaction):
+    sender_balance = get_balance(transaction['sender'])
+    return sender_balance >= transaction['amount']
 
 
 def get_last_blockchain_value():
@@ -49,21 +74,30 @@ def add_transaction(recipient, amount=1.0, sender=owner):
         'recipient': recipient,
         'amount': amount
     }
-    open_transactions.append(transaction)
-    participants.add(sender)
-    participants.add(recipient)
+    if verify_transaction(transaction):
+        open_transactions.append(transaction)
+        participants.add(sender)
+        participants.add(recipient)
+        return True
+    return False
 
 
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
-
+    reward_transaction = {
+        'sender': 'MINING',
+        'recipient': owner,
+        'amount': MINING_REWARD
+    }
+    open_transactions.append(reward_transaction)
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
         'transactions': open_transactions
     }
     blockchain.append(block)
+    return True
 
 # duplicate code must to stay as function
 
@@ -122,10 +156,14 @@ while waiting_for_input:
         # like destructure of javascript
         recipient, amount = get_transaction_value()
         # add the transaction to blockchain
-        add_transaction(recipient=recipient, amount=amount)
+        if add_transaction(recipient=recipient, amount=amount):
+            print('Added transaction')
+        else:
+            print('Transaction failed')
         print(open_transactions)
     elif user_choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         print_blocks()
     elif user_choice == '4':
@@ -147,6 +185,8 @@ while waiting_for_input:
         print_blocks()
         print('Invalid chain!')
         break
+
+    print(get_balance(owner))
 else:
     print('User left!')
 
